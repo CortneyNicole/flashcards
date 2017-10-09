025846fd8063740c41156cd5773a7f50fd192256
Cortney Scott <cortneynicolescott@gmail.com>
2017-10-09 11:59:55 -0700

    update deck with description, make deck routes and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009173154) do
+ActiveRecord::Schema.define(version: 20171009185137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171009173154) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "rounds", force: :cascade do |t|
